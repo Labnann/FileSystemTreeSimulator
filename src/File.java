21c56cac5e11e40e8a653497af7cc1080c9a9b05
Labnan <khalidmasum@iut-dhaka.edu>
2021-08-27 23:07:58 +0600

    Move File System Element Abstract class to a different file

--- src/File.java
+++ src/File.java
@@ -1,27 +1,3 @@
-abstract  class FileSystemElement implements ClickableNode{
-    String name;
-    String type;
-
-    FileSystemElement(String  name, String type){
-        this.name = name;
-        this.type = type;
-    }
-
-    @Override
-    public String getData() {
-        return ("Name: "+this.name+", Type: "+this.type);
-    }
-
-
-    @Override
-    public void singleClick() {
-        System.out.println("Name: "+this.name+", Type: "+this.type);
-    }
-
-    public abstract void doubleClick();
-}
-
-
 public class File extends FileSystemElement {
 
 
